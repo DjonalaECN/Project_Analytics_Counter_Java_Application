015bdf729cf6d1d242c3d714f6833956602a8613
Djonala <johanna.tristan@gmail.com>
2020-12-03 10:51:46 +0100

    Ajout des fonctionnalités de tri par compte et par ordre alphabétique

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounterProgram.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounterProgram.java
@@ -1,9 +1,13 @@
 package com.hemebiotech.analytics;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.TreeMap;
 
 public class AnalyticsCounterProgram {
     ISymptomReader reader;
-
+    ISymptomSort sorter;
 
     /**
      * Constructor of the class AnalyticsCounterProgram
@@ -11,14 +15,17 @@ public class AnalyticsCounterProgram {
      */
     public AnalyticsCounterProgram() {
         this.reader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
+        this.sorter = new SortSymptomData();
 
     }
 
     /**
      * Function who recover, sort and write a list of symptoms with the attached count
      */
-    public void start() {
+    public void start() throws IOException {
         ArrayList<String> listSymptoms;
+        TreeMap<String, Integer> listSymptomsClean;
         listSymptoms = (ArrayList<String>) this.reader.GetSymptoms();
+        listSymptomsClean = this.sorter.CleanSymptoms(listSymptoms);
     }
 }
